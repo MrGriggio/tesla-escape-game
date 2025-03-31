693e87e972ad9767d7d2856cefb4758f31e7417e
Mr Griggio <mrgriggio@gmail.com>
2025-04-01 00:33:17 +0200

    Add Vite configuration

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+export default {
+  server: {
+    port: 8080
+  },
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
+}
